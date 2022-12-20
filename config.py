8a26efe761e6dad19a32715d717466e83d4e856b
Rishii7205 <118364756+Rishii7205@users.noreply.github.com>
2022-12-20 17:24:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,13 +6,13 @@ if os.path.exists("local.env"):
     load_dotenv("local.env")
 
 
-API_ID = int(getenv("API_ID", "6435225")) #optional
-API_HASH = getenv("API_HASH", "") #optional
+API_ID = int(getenv("API_ID", "25394191")) #optional
+API_HASH = getenv("API_HASH", "256fcc3280c94ecda7dee238a08c02c7") #optional
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
-OWNER_ID = int(getenv("OWNER_ID", ""))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5430528255").split()))
+OWNER_ID = int(getenv("OWNER_ID", "5697261678"))
 MONGO_URL = getenv("MONGO_URL")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
+BOT_TOKEN = getenv("BOT_TOKEN", "5888020629:AAGrGHbIV4lO_25ti6KGcNdy12Atm3X62as")
 ALIVE_PIC = getenv("ALIVE_PIC")
 ALIVE_TEXT = getenv("ALIVE_TEXT")
 PM_LOGGER = getenv("PM_LOGGER")
@@ -21,7 +21,7 @@ GIT_TOKEN = getenv("GIT_TOKEN") #personal access token
 REPO_URL = getenv("REPO_URL", "https://github.com/ITZ-ZAID/ZAID-USERBOT")
 BRANCH = getenv("BRANCH", "master") #don't change
  
-STRING_SESSION1 = getenv("STRING_SESSION1", "")
+STRING_SESSION1 = getenv("STRING_SESSION1", "BQAg6gSl7th0nqokOTATzeBgln4x2Cul2Y2mdmKfQiIxXmAJoiWqQ64EkiQ27U4ASVYQsN-UeCphJaA21_7Uu36b7YFXjMtJ1rUZeq7KYAI5ludi2OznC_aPyLjT0Xx3g83B_275JfJl-DG4B8cgJ41Ul-zDieBRFvEXC4C1OXliOY4qjJxsCkaEqwEJ23uZuItbvUQru0WH0Xx7jvJ9QffciafgYCsrgkGTnmUmCYwgV36FdmwcJ16FC6p9rTccbqrG1L5tJ2Q7CeBtDDPOFzQEeHHQUmB92WmoNDOhEibJWXVjfTabF8rb5HmDWM8vaE-1OfyTCE2MzwgRz5NxcFopAAAAAVOVUG4A")
 STRING_SESSION2 = getenv("STRING_SESSION2", "")
 STRING_SESSION3 = getenv("STRING_SESSION3", "")
 STRING_SESSION4 = getenv("STRING_SESSION4", "")
