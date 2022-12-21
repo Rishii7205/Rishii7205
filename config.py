9721c5830304e8601863ea328c54f5fe4f734e14
Rishii7205 <118364756+Rishii7205@users.noreply.github.com>
2022-12-21 08:42:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,10 +7,10 @@ if os.path.exists("local.env"):
 
 
 API_ID = int(getenv("API_ID", "23043800")) #optional
-API_HASH = getenv("API_HASH", "256fcc3280c94ecda7dee238a08c02c7") #optional
+API_HASH = getenv("API_HASH", "611013e1151a9b80deed3359e53ac853") #optional
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5430528255").split()))
-OWNER_ID = int(getenv("OWNER_ID", "5697261678"))
+OWNER_ID = int(getenv("OWNER_ID", "5662440819"))
 MONGO_URL = getenv("MONGO_URL", "mongodb+srv://Yatoo99:Yatoo99@cluster0.3gj7zms.mongodb.net/?retryWrites=true&w=majority")
 BOT_TOKEN = getenv("BOT_TOKEN", "5862385277:AAHJ_pleWdwUYQtE1aoxC25YVhGQV20hCJI")
 ALIVE_PIC = getenv("ALIVE_PIC")
